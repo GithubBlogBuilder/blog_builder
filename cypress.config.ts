7670f8630163db739331fe0795ff146b22e12cc6
AlbertDoggyLin <albertlin2468@gmail.com>
2024-06-05 19:33:54 +0800

    login frontend test complete

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "cypress";
-console.log(process.env.TEST_ACCESS_TOKEN)
+
 export default defineConfig({
   e2e: {
     baseUrl:'http://localhost:3000',
+    defaultCommandTimeout: 8000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/loginTest.cy.ts
+++ cypress/e2e/loginTest.cy.ts
@@ -1,26 +1,38 @@
-import { invalid_login, pages } from "./plugin";
+import { invalid_login, login, logout, pages } from "./plugin";
 
-describe('Test login', () => {
+describe('Test with logout first', () => {
+    beforeEach(()=>{ logout(); })
     it('Test access token', ()=>{
         cy.setCookie('access_token', Cypress.env('test_access_token'));
         cy.visit('auth/login');
         cy.location('pathname').should('not.be', '/auth/login');
-    })
+    });
     it('Test oauth back success', () => {
         cy.visit(`/auth/login/callback?code=${Cypress.env('test_oauth_code')}`);
         cy.location('pathname').should('not.be', '/auth/login/callback');
         cy.getCookie('access_token').its('value').should('eq', Cypress.env('test_access_token'));
-        cy.get('#user-name').its('innerHTML').should('eq', Cypress.env('test_user_name'))
-    });
-    it('Test logout', () => {
-        cy.get('#action_list').click();
-        cy.get('div[innerHTML="登出"]').click();
-        cy.getCookie('access_token').should('eq', null);
+        cy.get(`#action_list`);
     });
     it('Test oauth back failed', () => {
-        cy.visit(`/auth/login/callback?code=${Cypress.env('test_failed_code')}`);
+        cy.visit(`/auth/login/callback?code=${Cypress.env('test_invalid_token')}`);
         cy.location('pathname').should('not.be', '/auth/login/callback');
         cy.location('pathname').should('eq', '/auth/login');
+        cy.getCookie('access_token').should('eq', null);
+    });
+});
+
+describe('Test with login first', ()=>{
+    beforeEach(()=>{
+        login();
+    })
+    it('Test logout', () => {
+        cy.setCookie('access_token', Cypress.env('test_access_token'));
+        cy.visit('/dashboard')
+        cy.get(`div:contains('${Cypress.env('test_user_name')}')`)
+        cy.get(`#action_list`).find('button').eq(0).click();
+        cy.get("div:contains('登出')")
+        cy.get("div:contains('登出')").contains(/^登出$/).eq(0).click();
+        cy.location('pathname').should('eq', '/')
         cy.getCookie('access_token').should('eq', null);
     });
 })
