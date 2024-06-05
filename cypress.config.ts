184cdebf35e789a3f1681bf9dc47e6e096542141
AlbertDoggyLin <albertlin2468@gmail.com>
2024-06-05 17:20:19 +0800

    env file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "cypress";
-
+console.log(process.env.TEST_ACCESS_TOKEN)
 export default defineConfig({
   e2e: {
     baseUrl:'http://localhost:3000',
-    env:{
-      test_access_token: process.env.TEST_ACCESS_TOKEN,
-      test_oauth_code: 'super_safe_test_code',
-      test_failed_code: 'failed_oauth_code',
-      test_user_name: 'Cutiemango'
-    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
