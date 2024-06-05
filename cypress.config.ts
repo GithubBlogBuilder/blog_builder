d742ea243c2e200f5f57ef9fe98cb9247ed5e23f
AlbertDoggyLin <albertlin2468@gmail.com>
2024-06-05 17:12:42 +0800

    emergency

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,10 +4,10 @@ export default defineConfig({
   e2e: {
     baseUrl:'http://localhost:3000',
     env:{
-      test_access_token: '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',
+      test_access_token: process.env.TEST_ACCESS_TOKEN,
       test_oauth_code: 'super_safe_test_code',
       test_failed_code: 'failed_oauth_code',
-      test_user_name: 'quan0715'
+      test_user_name: 'Cutiemango'
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
