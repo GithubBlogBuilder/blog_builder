1433559013e1caa581953f8991b025dedc5f9442
AlbertDoggyLin <albertlin2468@gmail.com>
2024-06-05 17:04:37 +0800

    wrong code

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     baseUrl:'http://localhost:3000',
     env:{
       test_access_token: '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',
-      test_oauth_code: 'super_safe_code',
+      test_oauth_code: 'super_safe_test_code',
       test_failed_code: 'failed_oauth_code',
       test_user_name: 'quan0715'
     },
